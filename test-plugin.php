59590567e8a31e09d1e3f61ce2078944ea286917
Peter Sorensen <psorensen@openminds.com>
2016-09-10 08:24:44 -0700

    initial commit

---
+++ test-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Test Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     test-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Test_Plugin
+ */
---
+++ tests/test-sample.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Test_Plugin
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+		$this->assertTrue( false );
+		$this->assertTrue( true );
+	}
+}
